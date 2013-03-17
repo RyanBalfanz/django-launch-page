385910f29ee27ea1e39ea8631a77c0c7a4e306fe
Ryan Balfanz <ryan@ryanbalfanz.net>
2013-03-17 00:21:54 -0700

    Updated settings

--- example_project/example_project/settings/development.py
+++ example_project/example_project/settings/development.py
@@ -3,7 +3,20 @@ import dj_database_url
 from .base import *
 
 
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
 DATABASES['default'] =  dj_database_url.config()
+
+MIDDLEWARE_CLASSES += (
+	'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INSTALLED_APPS += (
+	'debug_toolbar',
+)
+
+INTERNAL_IPS = ('127.0.0.1',)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
