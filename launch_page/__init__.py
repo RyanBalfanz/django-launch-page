69c7ced2b302b60a773098dfde8b8873b04ccd63
Ryan Balfanz <ryan@ryanbalfanz.net>
2013-03-16 21:28:23 -0700

    Moved launch_page app

---
+++ launch_page/__init__.py
@@ -0,0 +1,14 @@
+"""
+django-launch-page
+
+A launch page for a Django project to collect e-mail addresses and more.
+"""
+
+VERSION = (0, 0, 1)
+
+__version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
+__author__ = "Ryan Balfanz"
+__contact__ = "ryan@ryanbalfanz.net"
+__homepage__ = "http://github.com/RyanBalfanz/django-launch-page"
+__docformat__ = "markdown"
+# __license__ = "BSD (3 clause)"
