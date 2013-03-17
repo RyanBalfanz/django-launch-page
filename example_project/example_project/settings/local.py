c3676798a8e53c3317a068cc4881b48a91169408
Ryan Balfanz <ryan@ryanbalfanz.net>
2013-03-17 00:20:15 -0700

    Updated settings

--- example_project/example_project/settings/local.py
+++ example_project/example_project/settings/local.py
@@ -1,16 +1,19 @@
 from .base import *
 
 
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'example_project.db',
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': 'example_project.db',
+		# The following settings are not used with sqlite3:
+		'USER': '',
+		'PASSWORD': '',
+		'HOST': '',
+		'PORT': '',
+	}
 }
 
 MIDDLEWARE_CLASSES += (
