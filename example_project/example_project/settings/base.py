3daf31ec03bbfa2db93b52787cc4e61eb3edfafa
Ryan Balfanz <ryan@ryanbalfanz.net>
2013-03-17 00:19:27 -0700

    Moved local settings to the right file

--- example_project/example_project/settings/base.py
+++ example_project/example_project/settings/base.py
@@ -3,16 +3,9 @@ import os
 from .factory import *
 
 
-MIDDLEWARE_CLASSES += (
-	'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
 INSTALLED_APPS += (
 	'django.contrib.admin',
 	'django.contrib.admindocs',
 	'south',
-	'debug_toolbar',
 	'launch_page',
 )
-
-INTERNAL_IPS = ('127.0.0.1',)
--- example_project/example_project/settings/local.py
+++ example_project/example_project/settings/local.py
@@ -12,3 +12,13 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+MIDDLEWARE_CLASSES += (
+	'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INSTALLED_APPS += (
+	'debug_toolbar',
+)
+
+INTERNAL_IPS = ('127.0.0.1',)
