b3c057e90621dee6a5b1440f9c4e61df85764496
Ryan Balfanz <ryan@ryanbalfanz.net>
2013-03-17 00:20:06 -0700

    Added ADMINS

--- example_project/example_project/settings/base.py
+++ example_project/example_project/settings/base.py
@@ -6,6 +6,12 @@ from .factory import *
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
+ADMINS = (
+    ('Ryan Balfanz', 'ryan@ryanbalfanz.net'),
+)
+
+MANAGERS = ADMINS
+
 INSTALLED_APPS += (
 	'django.contrib.admin',
 	'django.contrib.admindocs',
