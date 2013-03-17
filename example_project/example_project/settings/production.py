259ad04d019a0c3e3c2afdd586aa640dfb1905e5
Ryan Balfanz <ryan@ryanbalfanz.net>
2013-03-17 00:28:52 -0700

    Added production settings

---
+++ example_project/example_project/settings/production.py
@@ -0,0 +1,9 @@
+import dj_database_url
+
+from .base import *
+
+
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
