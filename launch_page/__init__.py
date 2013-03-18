46c33b23b7e4562ad9887a5c593e9e64175ed758
Ryan Balfanz <ryan@ryanbalfanz.net>
2013-03-17 17:22:37 -0700

    Bumped version to v0.1.1

--- launch_page/__init__.py
+++ launch_page/__init__.py
@@ -4,7 +4,7 @@ django-launch-page
 A launch page for a Django project to collect e-mail addresses and more.
 """
 
-VERSION = (0, 1, 0)
+VERSION = (0, 1, 1)
 
 __version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
 __author__ = "Ryan Balfanz"
